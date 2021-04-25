ff0e81cffacfc88235f482529a5a7f7bde75f107
AjaideepSingh <c17720695@mytudublin.ie>
2021-04-25 22:32:23 +0100

    Finsihed software component

--- src/Driver.java
+++ src/Driver.java
@@ -7,67 +7,90 @@ public class Driver {
     private final ArrayList<Topic> topics = new ArrayList<>();
 
     public Driver() {
-//        String selection;
-//        System.out.println("Welcome to Student Grade Manager!");
-//        do {
-//            do {
-//                System.out.println("1.Create a student Grade Card");
-//                System.out.println("2.List of all rubrics");
-//                System.out.println("3.Get rubrics by topic name");
-//                System.out.println("4.Get student grades");
-//                System.out.println("5.View Summary of all student grades");
-//                System.out.println("6.Exit application");
-//                boolean flag;
-//                do {
-//                    System.out.print("Select menu option: ");
-//                    selection = scanner.next();
-//                    try {
-//                        Integer.parseInt(selection);
-//                        flag = false;
-//                    } catch (NumberFormatException e) {
-//                        System.out.println("Enter only integer value");
-//                        flag = true;
-//                    }
-//                } while (flag);
-//                if (Integer.parseInt(selection) < 1 || Integer.parseInt(selection) > 6) {
-//                    System.out.println("Error occurred: Choose valid selection");
-//                } else if (Integer.parseInt(selection) == 1) {
-//                    createStudentGradeCard();
-//                } else if (Integer.parseInt(selection) == 2) {
-//                    listAllRubrics();
-//                } else if (Integer.parseInt(selection) == 3) {
-//                    System.out.println("Enter topic name: ");
-//                    String name = scanner.next();
-//                    for(int i = 0; i < topics.size(); i++) {
-//                        if(topics.get(i).getTopicName().equalsIgnoreCase(name)) {
-//                            getRubricByTopicName(name);
-//                        } else {
-//                            System.out.println("Error occurred: topic doesn't exist");
-//                            break;
-//                        }
-//                    }
-//                } else if (Integer.parseInt(selection) == 4) {
-//                    System.out.println("Enter student first name: ");
-//                    String name = scanner.next();
-//                    for(int i = 0; i < students.size(); i++) {
-//                        if(students.get(i).getFirstName().equalsIgnoreCase(name)) {
-//                            getSpecificStudentGrade(name);
-//                        } else {
-//                            System.out.println("Error occurred: student doesn't exist");
-//                            break;
-//                        }
-//                    }
-//                } else if (Integer.parseInt(selection) == 5) {
-//                    calculations();
-//                } else if (Integer.parseInt(selection) == 6) {
-//                    System.out.println("Goodbye!");
-//                }
-//            } while (Integer.parseInt(selection) < 1 || Integer.parseInt(selection) > 6);
-//        } while (Integer.parseInt(selection) != 6);
+        String selection;
+        System.out.println("Welcome to Student Grade Manager!");
+        do {
+            do {
+                System.out.println("1.Create a student Grade Card");
+                System.out.println("2.List of all rubrics");
+                System.out.println("3.Get rubrics by topic name");
+                System.out.println("4.Get student grades");
+                System.out.println("5.View Summary of all student grades");
+                System.out.println("6.Exit application");
+                boolean flag;
+                do {
+                    System.out.print("Select menu option: ");
+                    selection = scanner.next();
+                    try {
+                        Integer.parseInt(selection);
+                        flag = false;
+                    } catch (NumberFormatException e) {
+                        System.out.println("Enter only integer value");
+                        flag = true;
+                        break;
+                    }
+                } while (flag);
+                if (Integer.parseInt(selection) < 1 || Integer.parseInt(selection) > 6) {
+                    System.out.println("Error occurred: Choose valid selection");
+                } else if (Integer.parseInt(selection) == 1) {
+                    createStudentGradeCard();
+                } else if (Integer.parseInt(selection) == 2) {
+                    if (rubrics.isEmpty()) {
+                        System.out.println("Error no data available!");
+                    } else {
+                        listAllRubrics();
+                    }
+                } else if (Integer.parseInt(selection) == 3) {
+                    if (topics.isEmpty()) {
+                        System.out.println("Error no data available!");
+                    } else {
+                        System.out.print("Enter topic name: ");
+                        String name = scanner.next();
+                        boolean found = false;
+                        for (Topic topic : topics) {
+                            found = topic.getTopicName().equalsIgnoreCase(name);
+                            if (found = true) {
+                                getRubricByTopicName(name);
+                                break;
+                            } else if (found = false) {
+                                System.out.println("Error occurred: topic doesn't exist");
+                            }
+                        }
+                    }
+                } else if (Integer.parseInt(selection) == 4) {
+                    if (students.isEmpty()) {
+                        System.out.println("Error no data available!");
+                    } else {
+                        System.out.print("Enter student first name: ");
+                        String name = scanner.next();
+                        boolean found = false;
+                        for (Student student : students) {
+                            found = student.getFirstName().equalsIgnoreCase(name);
+                            if (found = true) {
+                                getSpecificStudentGrade(name);
+                                break;
+                            } else if (found = false) {
+                                System.out.println("Error occurred: student doesn't exist");
+                            }
+                        }
+                    }
+                } else if (Integer.parseInt(selection) == 5) {
+                    if (students.isEmpty()) {
+                        System.out.println("Error no data available!");
+                    } else {
+                        calculations();
+                    }
+                } else if (Integer.parseInt(selection) == 6) {
+                    System.out.println("Goodbye!");
+                }
+            } while (Integer.parseInt(selection) < 1 || Integer.parseInt(selection) > 6);
+        } while (Integer.parseInt(selection) != 6);
     }
 
     public void createStudentGradeCard() {
         int criteriaAmount;
+        ArrayList<Rubric> rubrics2 = new ArrayList<>();
+        rubrics2.clear();
         System.out.print("Enter first name: ");
         String firstName = scanner.next();
         System.out.print("Enter last name: ");
@@ -93,44 +116,44 @@ public class Driver {
                 }
             } while (criteriaValue < 1 || criteriaValue > 5);
             Rubric rubric = new Rubric(criteriaName, criteriaValue);
+            rubrics2.add(rubric);
             rubrics.add(rubric);
         }
-        Topic topic = new Topic(topicName, rubrics);
+        Topic topic = new Topic(topicName, rubrics2);
         topics.add(topic);
         Student student = new Student(firstName, lastName, topic);
         students.add(student);
+        System.out.println("Student grade card created successfully!");
     }
 
     public void listAllRubrics() {
         for (Rubric rubric : rubrics) {
             System.out.println("Rubric name: " + rubric.getCriteria());
-            System.out.println("Rubric value: " + rubric.getValue());
         }
     }
 
     public void getRubricByTopicName(String name) {
         ArrayList<Rubric> rubricArrayList = new ArrayList<>();
-        for(int i = 0; i < topics.size(); i++) {
-            if(topics.get(i).getTopicName().equalsIgnoreCase(name)) {
-                System.out.println("Topic name: " + topics.get(i).getTopicName());
-                rubricArrayList = topics.get(i).getRubrics();
+        for (Topic topic : topics) {
+            if (topic.getTopicName().equalsIgnoreCase(name)) {
+                System.out.println("Topic name: " + topic.getTopicName());
+                rubricArrayList = topic.getRubrics();
                 break;
             }
         }
-        for(int e = 0; e < rubricArrayList.size(); e++) {
-            System.out.println("Rubric name: " + rubricArrayList.get(e).getCriteria());
-            System.out.println("Rubric name: " + rubricArrayList.get(e).getValue());
+        for (Rubric rubric : rubricArrayList) {
+            System.out.println("Rubric name: " + rubric.getCriteria());
         }
     }
 
     public void getSpecificStudentGrade(String name) {
-        for(int i = 0; i < students.size(); i++) {
-            if (students.get(i).getFirstName().equalsIgnoreCase(name)) {
-                System.out.println("Student name: " + students.get(i).getFirstName() + " " + students.get(i).getLastName());
-                System.out.println("Topic: " + students.get(i).getTopic().getTopicName());
-                for (int e = 0; e < students.get(i).getTopic().getRubrics().size(); e++) {
-                    System.out.println("Rubric name: " + students.get(i).getTopic().getRubrics().get(e).getCriteria());
-                    System.out.println("Rubric name: " + students.get(i).getTopic().getRubrics().get(e).getValue());
+        for (Student student : students) {
+            if (student.getFirstName().equalsIgnoreCase(name)) {
+                System.out.println("Student name: " + student.getFirstName() + " " + student.getLastName());
+                System.out.println("Topic: " + student.getTopic().getTopicName());
+                for (int e = 0; e < student.getTopic().getRubrics().size(); e++) {
+                    System.out.println("Rubric name: " + student.getTopic().getRubrics().get(e).getCriteria());
+                    System.out.println("Rubric value: " + student.getTopic().getRubrics().get(e).getValue());
                 }
             }
         }
@@ -139,12 +162,10 @@ public class Driver {
     public void calculations() {
         for (Student student : students) {
             System.out.println("Student name: " + student.getFirstName() + " " + student.getLastName());
-            for (int e = 0; e < student.getTopic().getRubrics().size(); e++) {
-                calculateAverage(student.getTopic().getRubrics());
-                calculateMinimumRubricScore(student.getTopic().getRubrics());
-                calculateMaximumRubricScore(student.getTopic().getRubrics());
-                calculateStandardDeviation(student.getTopic().getRubrics());
-            }
+            calculateAverage(student.getTopic().getRubrics());
+            calculateMinimumRubricScore(student.getTopic().getRubrics());
+            calculateMaximumRubricScore(student.getTopic().getRubrics());
+            calculateStandardDeviation(student.getTopic().getRubrics());
         }
         System.out.println("Calculations based on all students!");
         calculateTotalAverage(rubrics);
@@ -188,7 +209,7 @@ public class Driver {
         double standardDeviation = 0.0;
         int sum = results.stream().mapToInt(Integer::intValue).sum();
         double mean = sum / results.size();
-        for(double num: results) {
+        for (double num : results) {
             standardDeviation += Math.pow(num - mean, 2);
         }
         System.out.println("Standard deviation: " + standardDeviation / results.size());
@@ -230,7 +251,7 @@ public class Driver {
         double standardDeviation = 0.0;
         int sum = results.stream().mapToInt(Integer::intValue).sum();
         double mean = sum / results.size();
-        for(double num: results) {
+        for (double num : results) {
             standardDeviation += Math.pow(num - mean, 2);
         }
         System.out.println("Total standard deviation: " + standardDeviation / results.size());
